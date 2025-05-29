ff6bb6c8935ea656c9a0c2b80637ef644392180f
Moritz2k1 <moritz.gutschi@outlook.com>
2025-05-29 23:30:25 +0200

    Add background and more lvl elements

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 @onready var animated_sprite  = $AnimatedSprite2D
 
-const SPEED = 130.0
+const SPEED = 150.0
 const JUMP_VELOCITY = -300.0
 
 func _physics_process(delta: float) -> void:
