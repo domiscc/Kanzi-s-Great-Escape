81d989cb0877f8c9fea07c0cd4e8531b82c91249
Moritz2k1 <moritz.gutschi@outlook.com>
2025-05-29 15:46:39 +0200

    Add outline for first lvl, tiles and character sprites
    Monkey can move with the proper animation. Create first outline of the first jungle themed level.

---
+++ scripts/player.gd
@@ -0,0 +1,39 @@
+extends CharacterBody2D
+
+@onready var animated_sprite  = $AnimatedSprite2D
+
+const SPEED = 130.0
+const JUMP_VELOCITY = -300.0
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction: -1, 0, 1
+	var direction = Input.get_axis("ui_left", "ui_right")
+	
+	# Flip the Sprite
+	if direction > 0:
+		animated_sprite.flip_h = false
+	elif direction < 0:
+		animated_sprite.flip_h = true
+	
+	# Play animations
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.stop()
+		else:
+			animated_sprite.play("move_right")
+			
+		
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
