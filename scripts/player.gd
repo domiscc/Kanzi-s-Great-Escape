163813c41705d28e4a24928e45268fc074bfa7b3
Moritz2k1 <moritz.gutschi@outlook.com>
2025-06-18 11:39:29 +0200

    Add coin score (not done yet) and climbing

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,45 +1,65 @@
 extends CharacterBody2D
 
-# Imports
-@onready var animated_sprite  = $AnimatedSprite2D
+@onready var animated_sprite = $AnimatedSprite2D
 
 # Constants
 const SPEED = 150.0
 const JUMP_VELOCITY = -300.0
+const WALL_CLIMB_SPEED = -50.0
+const MAX_CLIMB_TIME = 1.5
 
 # Variables
-var currentHealth: int = 3
+var current_health: int = 3
+var coin_counter = 0
+var climb_time_left: float = MAX_CLIMB_TIME
 
 func _physics_process(delta: float) -> void:
 	
-	# Add the gravity.
-	if not is_on_floor():
+	var dir = Input.get_axis("move_left", "move_right")
+
+	# Handle gravity or climbing
+	var climbing = false
+	if is_on_wall() and dir != 0 and climb_time_left > 0.0:
+		velocity.y = WALL_CLIMB_SPEED
+		climb_time_left -= delta
+		climbing = true
+	elif not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
+	# Refill climb time when on ground
+	if is_on_floor():
+		climb_time_left = MAX_CLIMB_TIME
+
+	# Handle jump
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction: -1, 0, 1
-	var direction = Input.get_axis("move_left", "move_right")
-	
-	# Flip the Sprite
-	if direction > 0:
+	# Flip the sprite
+	if dir > 0:
 		animated_sprite.flip_h = false
-	elif direction < 0:
+	elif dir < 0:
 		animated_sprite.flip_h = true
-	
+
 	# Play animations
 	if is_on_floor():
-		if direction == 0:
+		if dir == 0:
 			animated_sprite.stop()
 		else:
 			animated_sprite.play("move_right")
-			
-	if direction:
-		velocity.x = direction * SPEED
+
+	# Horizontal movement
+	if dir:
+		velocity.x = dir * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
 	
+	
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	if area.is_in_group("coin"):
+		set_coin(coin_counter + 1)
+		print(coin_counter)
+
+func set_coin(new_coin_count: int) -> void:
+	coin_counter = new_coin_count
