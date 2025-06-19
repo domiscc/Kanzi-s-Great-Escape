fa0591011a1cbab79a384e40eb29c8fc2461e833
Moritz2k1 <moritz.gutschi@outlook.com>
2025-06-19 13:38:26 +0200

    Add Mushroom and Flying enemies and level end
    Game has 2 more enemies now. A mushroom enemy and a flying enemy + the end of the level currently only restarts the scene

---
+++ scripts/level_end.gd
@@ -0,0 +1,25 @@
+extends Area2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		end_level()
+		
+		
+func end_level():
+	get_tree().reload_current_scene()
+	#get_tree().change_scene_to_file("res://scenes/WinScreen.tscn")  # Replace with your next scene
+
+	
